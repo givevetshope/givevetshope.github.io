fa66a7dc300a6431a7c4ddae604a049be6fe4634
clintdale <clintdaletagabucba@gmail.com>
2020-03-22 22:44:44 -0500

    added deleted pictures back with case sensitive extensions

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,32 +2,31 @@
 
       var imgThumb = ['img4.jpg', 'img6.jpg',
           'img7.jpeg',
-          'img9.jpg',
-          'img10.jpg',
-          'img11.jpg',
-          'img12.jpeg',
           'img13.jpg',
           'img14.jpg',
-          'img15.jpg',
-          'img16.jpg',
-          'img17.jpeg',
-          'img18.jpg',
-          'img19.jpeg',
           'img20.jpeg',
-          'img21.jpeg',
+          'img17.jpeg',
           'img22.jpg',
-          'img23.jpeg',
+          'img19.jpeg', 
+          'img15.jpeg',         
+          'img18.jpeg',
           'img24.jpg',
           'img25.jpg',
           'img26.jpg',
           'img27.jpg',
+          'img23.jpeg',
+          'img10.jpg',
+          'img11.jpg',
+          'img12.jpeg',          
+          'img9.jpg',
+          'img21.jpeg',
       ]
 
 
 
-      for (i = 0; i < 22; i++) {
+      for (i = 0; i < 21; i++) {
           document.getElementById('thumb'.concat(i)).src = "assets/img/" + imgThumb[i];
-          document.getElementById('thumb'.concat(i)).setAttribute("class", "img-thumbnail img-gallery modal-hover-opacity");
+          document.getElementById('thumb'.concat(i)).setAttribute("class", "img-thumbnail img-gallery modal-hover-opacity shadow");
           document.getElementById('thumb'.concat(i)).setAttribute("style", "width:300px; margin-top: 20px");
       }
 
